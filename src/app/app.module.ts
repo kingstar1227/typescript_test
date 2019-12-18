69f0d2a1ed782e888a0d1a41e42ee5c5585ff242
kingstar1227 <tedd8785@gmail.com>
2019-12-18 18:53:34 -0500

    add pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CommitListComponent } from './commit-list/commit-list.component';
@@ -17,8 +18,10 @@ const routes: Routes = [
     CommitListComponent
   ],
   imports: [
-    NgbModule,
     BrowserModule,
+    NgbModule,
+    FormsModule,
+    NgbPaginationModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
   ],
--- src/app/commit-list/commit-list.component.ts
+++ src/app/commit-list/commit-list.component.ts
@@ -9,10 +9,22 @@ import { CommitService } from '../commit.service';
 export class CommitListComponent implements OnInit {
 
   constructor(private commitService: CommitService) { }
-  commits = [];
+
+  commitList = [];
+  page = 1;
+  pageSize = 5;
+  collectionSize = this.commits.length || 0;
+
+  get commits(): any[] {
+    return this.commitList.map((commit, i) => ({id: i + 1, ...commit}))
+    .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
 
   ngOnInit() {
-
+    this.commitService.listCommits().subscribe((commits: any[]) => {
+      this.collectionSize = commits.length;
+      this.commitList = commits;
+    })
   }
 
 }
